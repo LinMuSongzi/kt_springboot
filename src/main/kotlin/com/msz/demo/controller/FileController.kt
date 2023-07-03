fabf06f4f1e3dea10556a7aba49445df3d46b5a9
linhui <536451207@qq.com>
2023-07-03 23:56:23 +0800

    1，新增websocket

--- src/main/kotlin/com/msz/demo/controller/FileController.kt
+++ src/main/kotlin/com/msz/demo/controller/FileController.kt
@@ -1,11 +1,15 @@
 package com.msz.demo.controller
 
+import com.msz.demo.NonStaticResourceHttpRequestHandler
 import com.msz.demo.bean.DiskInfo
 import com.msz.demo.bean.FileInfo
 import com.msz.demo.info.respone.RespondInfo
 import com.msz.demo.info.respone.simple
 import com.msz.demo.service.impl.FileService
+import org.apache.tomcat.util.http.fileupload.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringBootExceptionReporter
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.InputStreamResource
 import org.springframework.core.io.Resource
@@ -14,18 +18,22 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.InputStream
+import java.io.*
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @RestController
+//@AllArgsConstructor
 class FileController {
 
     @Autowired
-    lateinit var service : FileService
+    lateinit var service: FileService
 
     @PostMapping("/disk/search")
     fun searchDisk(@RequestParam("acceseKey") acceseKey: String): RespondInfo<List<DiskInfo>> {
@@ -33,14 +41,21 @@ class FileController {
     }
 
     @PostMapping("/disk/files")
-    fun getDirFiles(@RequestParam("acceseKey") acceseKey: String, @RequestParam("path") path: String): RespondInfo<List<FileInfo>> {
-        return simple(data = service.loadDirFiles(acceseKey,path))
+    fun getDirFiles(
+        @RequestParam("acceseKey") acceseKey: String,
+        @RequestParam("path") path: String
+    ): RespondInfo<List<FileInfo>> {
+        return simple(data = service.loadDirFiles(acceseKey, path))
     }
 
     @GetMapping("/disk/file2")
     @Throws(IOException::class)
-    fun loadFile(@RequestParam("acceseKey") acceseKey: String, @RequestParam("path") path: String): ResponseEntity<Resource?>? {
+    fun loadFile(
+        @RequestParam("acceseKey") acceseKey: String,
+        @RequestParam("path") path: String
+    ): ResponseEntity<Resource?>? {
         service.findPath(acceseKey, path)?.apply {
+
             // 根据文件名获取文件流
             val inputStream: InputStream = FileInputStream(this)
             // 创建文件流资源对象
@@ -57,6 +72,67 @@ class FileController {
         return null
     }
 
+
+    @Autowired
+    private var nonStaticResourceHttpRequestHandler: NonStaticResourceHttpRequestHandler? = null
+
+    /**
+     * 预览视频文件, 支持 byte-range 请求
+     */
+    @GetMapping("/disk/file/video")
+    @Throws(Exception::class)
+    fun videoPreview(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        @RequestParam("acceseKey") acceseKey: String,
+        @RequestParam("path") path: String
+    ) {
+        val path = service.findPath(acceseKey, path) ?: return
+        val filePath: Path = Paths.get(path.absolutePath)
+        if (Files.exists(filePath)) {
+            val mimeType = Files.probeContentType(filePath)
+            if (!mimeType.isNullOrEmpty()) {
+                response.contentType = mimeType
+            }
+            request.setAttribute(NonStaticResourceHttpRequestHandler.ATTR_FILE, filePath)
+
+//            Sprin
+
+            nonStaticResourceHttpRequestHandler!!.handleRequest(request, response)
+        } else {
+            response.status = HttpServletResponse.SC_NOT_FOUND
+            response.characterEncoding = StandardCharsets.UTF_8.toString()
+        }
+    }
+
+    //    @ApiOperation("单个MP4播放")
+//    @GetMapping(value = ["/disk/file/video"], produces = ["application/json;charset=utf-8"])
+//    fun aloneVideoPlay(
+//        request: HttpServletRequest?,
+//        @RequestParam("acceseKey") acceseKey: String, @RequestParam("path") path: String,
+//        response: HttpServletResponse
+//    ) {
+//        var `is`: InputStream? = null
+//        var os: OutputStream? = null
+//        try {
+//            response.contentType = "video/mp4"
+//            val file = service.findPath(acceseKey, path) ?: return
+//            response.addHeader("Content-Length", "" + file.length())
+//            `is` = FileInputStream(file)
+//            os = response.outputStream
+//            IOUtils.copy(`is`, os)
+//        } catch (e: Exception) {
+////            log.error("播放MP4失败", e)
+//        } finally {
+//            if (null != os) {
+//                try {
+//                    os.close()
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//        }
+//    }
 
     fun export(file: File?): ResponseEntity<FileSystemResource?>? {
         if (file == null) {
